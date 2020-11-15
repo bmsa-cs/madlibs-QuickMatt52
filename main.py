1b97bd5811404606b80466fbbcbe1c4354a2ca0f
QuickMatt52 <73911155+QuickMatt52@users.noreply.github.com>
2020-11-15 16:10:03 -0500

    Update main.py
    Need to help with this.

--- main.py
+++ main.py
@@ -5,3 +5,15 @@ Period/Core:
 
 
 """
+my_string = 'Hola,cómo estás?'
+print(my_string)
+
+name = input("What is your name?")
+num = int(input("What year were you born? "))
+print(f"Great! {name}!\nCompters still exists in that year! {num}!")
+# Hola is one of my strings
+my_string = 'Hola'
+print(my_string)
+#this is my .format
+ 
+ 
