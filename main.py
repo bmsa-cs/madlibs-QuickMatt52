acf3fe14235efa289cf665095b4adf7bd9235ee5
Matthew Gonczy <mgonczy24@biomedscienceacademy.org>
2020-11-19 18:27:34 +0000

    I think I am finished

--- main.py
+++ main.py
@@ -15,12 +15,12 @@ print(f"Great! {name}!\nCompters still exists in that year! {num}!")
 my_string = 'Hola'
 print(my_string)
 #this is my .format
-name = input("What is your name?")
-name = input("What is your name?")
-name = input("What is your name?")
-name = input("What is your name?")
-name = input("What is your name?")
-name = input("What is your name?")
-name = input("What is your name?")
+name = input("What is your favorite hoilday ?")
+name = input("What are you doing this hoilday?")
+name = input("What do you like to eat on thanksgiving ?")
+name = input("What do you want for Christmas ?")
+name = input("What is your favorite code to use ?")
+name = input("Do you think Gaming is fun ?")
+name = input("What is your best score in Gaming?")
 
 
